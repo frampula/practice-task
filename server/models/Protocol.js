2d59bb6fa4606a504099a9b2c3a510c1030bfab3
framupla <kidalo1337@gmail.com>
2024-07-31 15:02:10 +0200

    Initialize react app

---
+++ server/models/Protocol.js
@@ -0,0 +1,56 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Protocol extends Model {
+    static associate({ ParkOfficer, Image }) {
+      Protocol.belongsTo(ParkOfficer, {
+        foreignKey: 'officerId'
+      });
+
+      Protocol.hasMany(Image, {
+        foreignKey: 'protocolId'
+      });
+    }
+  }
+  Protocol.init({
+    serviceNotes: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    fineAmount: {
+      allowNull: false,
+      type: DataTypes.DOUBLE,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    violatorFullName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    violatorPassportNumber: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Protocol',
+    tableName: 'protocols',
+    underscored: true
+  });
+
+  return Protocol;
+}
